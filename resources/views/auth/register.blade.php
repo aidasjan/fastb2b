50142e22c0d2e8a790e0974c75b2be9ab8fd446f
unknown <aidas.jankauskas31@gmail.com>
2020-02-28 16:45:27 +0200

    user registration

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -5,19 +5,30 @@
 <div class='container'>
     <div class='row py-5'>
         <div class='col'>
-            <h1 class='text-uppercase'>Login</h1>
+            <h1 class='text-uppercase'>New user</h1>
         </div>
     </div>
         
     <div class='row py-3'>
         <div class='col'>
-            <form method="POST" action="{{ route('login') }}">
+            <form method="POST" action="{{ action('Auth\RegisterController@register') }}">
                 @csrf
+                <div class="form-group row">
+                    <label for="email" class="col-md-4 col-form-label text-md-right">Name</label>
+                    <div class="col-md-5">
+                        <input id="email" type="name" class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}" name="name" value="" placeholder="" required autofocus>
+                        @if ($errors->has('name'))
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $errors->first('name') }}</strong>
+                            </span>
+                        @endif
+                    </div>
+                </div>
                 <div class="form-group row">
                     <label for="email" class="col-md-4 col-form-label text-md-right">Email</label>
                     <div class="col-md-5">
                         <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="" placeholder="" required autofocus>
-                        @if ($errors->has('email_h'))
+                        @if ($errors->has('email'))
                             <span class="invalid-feedback" role="alert">
                                 <strong>{{ $errors->first('email') }}</strong>
                             </span>
@@ -33,6 +44,12 @@
                                 <strong>{{ $errors->first('password') }}</strong>
                             </span>
                         @endif
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label for="password_confirmation" class="col-md-4 col-form-label text-md-right">Confirm Password</label>
+                    <div class="col-md-5">
+                        <input id="password_confirmation" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" value="" name="password_confirmation" placeholder="" required>
                     </div>
                 </div>
                 <div class="form-group row">
--- routes/web.php
+++ routes/web.php
@@ -13,4 +13,11 @@
 
 Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
 Route::post('login', 'Auth\LoginController@login');
-Route::post('logout', 'Auth\LoginController@logout')->name('logout');
+Route::get('logout', 'Auth\LoginController@logout')->name('logout');
+
+Route::get('register', 'Auth\RegisterController@showRegistrationForm');
+Route::post('register', 'Auth\RegisterController@register');
+
+Route::get('dashboard', function(){ 
+    dd("connected");
+});
