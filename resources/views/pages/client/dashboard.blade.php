0fe3f27d8d45029489cc6deb54a052688cff81f5
Pocius Linas <linpoc3@ktu.lt>
2020-03-14 18:37:58 +0200

    Added user(client) information label

--- resources/views/pages/client/dashboard.blade.php
+++ resources/views/pages/client/dashboard.blade.php
@@ -3,8 +3,12 @@
 @section('content')
 
 <div class='container'>
-    
+    <div class='row py-5'>
+        <div class='col'>
+            <h5>{{auth()->user()->name}} | {{auth()->user()->email}}</h5>
+            <h1 class='text-uppercase'>Dashboard</h1>
+        </div>
+    </div>
 </div>
-
-                    
+             
 @endsection
